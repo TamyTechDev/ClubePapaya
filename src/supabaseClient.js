fb6a993f73d6226f0164584ccca663ba43ca8808
Tamy T. T. <tamyt.tech@gmail.com>
2026-08-25 16:03:51 -0300

    ajuste supabaseclient
    ajuste supabaseclient

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,6 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://zscxlfjzwfqiwquqjurq.supabase.co'
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_KdD41evoWP61t1ZsOE7wwA_rq7rIs4B'
 
-console.log('--- SUPABASE CLIENT INITIALIZED WITH URL ---', supabaseUrl)
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
